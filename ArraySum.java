41c90b2459e8d2dc2d3420548b260ec603800648
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-05-16 18:22:36 +0530

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class sumofintegersinsinglearray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		int n[]=new int[] {2,5,1,9,6};
+		int sum=0;
+		for(int i=0;i<n.length;i++) {
+			sum=sum+n[i];
+			
+		}
+		System.out.println(sum);
+	}
+
+}
