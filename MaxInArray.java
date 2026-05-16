790e3ec36a10a8b737660c798d940a9cb4825c21
Bonthu-Jaswini <bonthujaswinijaswini@gmail.com>
2026-05-16 20:06:24 +0530

    Update MaxInArray.java

--- MaxInArray.java
+++ MaxInArray.java
@@ -6,13 +6,17 @@ public class MaxofArray {
 		// TODO Auto-generated method stub
 		int n[] = new int[] { 2, 5, 1, 9, 6 };
 		int max = n[0];
-		for (int i = 0; i < n.length; i++) {
+		int i;
+		for (i = 0; i < n.length; i++) {
 			if (n[i] > max) {
 				max = n[i];
 			}
 
 		}
 
-		System.out.print(max);
+		System.out.println("Maximum value is:" + max);
+		System.out.println(" At Index: " + i);
+
 	}
+
 }
